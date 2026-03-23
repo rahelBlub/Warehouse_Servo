8eee41242842c51036c3e51b1cf6520efb5d1262
Rahel <rahel.grandi@web.de>
2026-03-23 14:20:04 +0100

    finished gpio

--- Warehouse_Servo/gpio.py
+++ Warehouse_Servo/gpio.py
@@ -17,8 +17,20 @@ def angle_to_percent(angle):
 GPIO.setmode(GPIO.BOARD) #use physical pin numbering/ Board numerotation mode
 GPIO.setwarnings(False) #disable warnings
 
+#use pin 12 for pwm signal
 PWM_GPIO = 12
 frequence = 50
 GPIO.setup(PWM_GPIO, GPIO.OUT) #set pin as output
 pwm = GPIO.PWM(PWM_GPIO, frequence) #create PWM instance with frequency
 
+#init at 0°
+pwm.start(angle_to_percent(0))
+time.sleep(1)
+
+#go at 90°
+pwm.ChangeDutyCycle(angle_to_percent(90))
+time.sleep(1)
+
+#close GPIO & cleanup
+pwm.stop()
+GPIO.cleanup()
