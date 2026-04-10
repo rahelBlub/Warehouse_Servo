0c20605fa420e70e078047542a5ddccd219ea648
Rahel <rahel.grandi@web.de>
2026-04-10 15:39:34 +0200

    middle pos test

--- Warehouse_Servo/Warehouse_Servo.py
+++ Warehouse_Servo/Warehouse_Servo.py
@@ -19,8 +19,7 @@ def on_connect(client, userdata, flags, rc):
         client.subscribe(TOPIC_CMD)
         # client.subscribe("$SYS/#")
         time.sleep(1)
-        servo.left()
-        #servo.middle() # starting with middle position
+        servo.middle() # starting with middle position
     else:
         print(f'Failed to connect, return code {rc}')
     # Subscribing in on_connect() means that if we lose the connection and
