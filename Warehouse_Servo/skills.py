6e69cf6bcee0167e968fc4db8e776b2c4a5ac146
Rahel <rahel.grandi@web.de>
2026-05-21 15:12:16 +0200

    left 1000 right 2000?

--- Warehouse_Servo/skills.py
+++ Warehouse_Servo/skills.py
@@ -12,24 +12,22 @@ class Skillset:
         print("Turning left")
         self.pi.set_servo_pulsewidth(self.pwm, 1000)
         time.sleep(0.5)
-        self.pi.set_servo_pulsewidth(self.pwm, 1200)
-        time.sleep(0.5)
         self.pi.set_servo_pulsewidth(self.pwm, 1500)
-        time.sleep(0.5)
-        self.pi.set_servo_pulsewidth(self.pwm, 1700)
-        time.sleep(0.5)
-        self.pi.set_servo_pulsewidth(self.pwm, 2000)
         time.sleep(0.5)
         print("finished skill left")
 
     def right(self):
         print("Turning right")
-
+        self.pi.set_servo_pulsewidth(self.pwm, 2000)
+        time.sleep(0.5)
+        self.pi.set_servo_pulsewidth(self.pwm, 1500)
+        time.sleep(0.5)
         print("finished skill right")
 
     def middle(self):
         print("Turning middle")
-
+        self.pi.set_servo_pulsewidth(self.pwm, 1500)
+        time.sleep(0.5)
         print("finished skill middle")
 
     def close(self):
