380f6100c097cc94ecaef1c71e61b607a2440e78
Rahel <rahel.grandi@web.de>
2026-08-26 15:34:30 +0200

    added .env

--- Warehouse_Servo/mqtt_config.py
+++ Warehouse_Servo/mqtt_config.py
@@ -1,10 +1,14 @@
 import paho.mqtt.client as mqtt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # ===== MQTT =====
-MQTT_BROKER= "141.19.44.65"
-MQTT_PORT= 18443
-MQTT_USER="suedzucker"
-MQTT_PASS="isomalt"
+MQTT_BROKER = os.getenv("MQTT_BROKER")
+MQTT_PORT = int(os.getenv("MQTT_PORT"))
+MQTT_USER = os.getenv("MQTT_USER")
+MQTT_PASS = os.getenv("MQTT_PASS")
 
 # ===== SERVO GPIO =====
 PWM_GPIO=12
